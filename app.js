b7f1b9846178364c22c09978cbd0e885d1b7fc2e
Shannon Peck <smpeck14@gmail.com>
2021-07-05 16:36:45 -0700

    Added comparison

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const btn = document.querySelectorAll(".userOption");
 let numBtn = btn.length;
-let uSelection;
-let computerSelection
 
 //Captures user's choice into a variable uSelection
 let showUserSelect = function () {
   uSelection = this.id;
+  return uSelection;
 }
 document.getElementById("rock").onclick = showUserSelect;
 document.getElementById("paper").onclick = showUserSelect;
 document.getElementById("scissors").onclick = showUserSelect;
 
 //Computer's selection based on randomly generated number
-let getCompSelection = function r () {
+let getCompSelection = function r() {
   let randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-
-  let computerSelection;
-  switch(randomNumber) {
+  switch (randomNumber) {
     case 1:
       computerSelection = "rock";
       break;
@@ -26,13 +23,33 @@ let getCompSelection = function r () {
     case 3:
       computerSelection = "scissors";
       break;
-  }
-  console.log(`User Chooses: ${uSelection}`)
+  };
+  console.log(`User Chooses: ${uSelection}`);
   console.log(`Computer Chooses: ${computerSelection}`);
-  return computerSelection
+  return computerSelection;
 };
 
+//Compare user choice with computer selection to determine game results
+function compareChoice(cSelect, uSelect) {
+  if (cSelect === uSelect) {
+    let gameResult = "It's a tie!"
+    console.log(gameResult);
+    return gameResult;
+  } else if (cSelect === "rock" && uSelect === "scissors" || cSelect === "paper" && uSelect === "rock" || cSelect === "scissors" && uSelect === "paper") {
+    let gameResult = "You lose!"
+    console.log(gameResult);
+    return gameResult;
+  } else {
+    let gameResult = "You win!"
+    console.log(gameResult);
+    return gameResult;
+  };
+};
 
+//Generates computer's response and game results when user makes selection
 for (let i = 0; i < numBtn; i++) {
-btn[i].addEventListener("click", getCompSelection)
+  btn[i].addEventListener("click", () => {
+    getCompSelection();
+    compareChoice(computerSelection, uSelection);
+  })
 };
